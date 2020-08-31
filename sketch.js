5a4e7b3cbec9a4931ba217b7d0af4905ddb04cdb
dvdjcw <70393261+dvdjcw@users.noreply.github.com>
2020-08-31 14:46:37 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -15,12 +15,22 @@ function draw() {
   background(255,255,255);
   movingrect.x=World.mouseX;
   movingrect.y=World.mouseY;
-  if(movingrect.x - fixedrect.x === fixedrect.width/2 + movingrect.width/2)
+  if(movingrect.x - fixedrect.x < fixedrect.width/2 + movingrect.width/2
+    && fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2
+  && movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2
+  && fixedrect.y - movingrect.y < fixedrect.height/2 + movingrect.height/2 )
   {
     fixedrect.shapeColor="red";
     movingrect.shapeColor="red";
   }  
+  else
+  {
+    fixedrect.shapeColor = "green";
+    movingrect.shapeColor = "green";
+  }
   drawSprites();
 }
+
+
 
 
