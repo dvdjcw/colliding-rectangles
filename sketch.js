bcd583a2cce9def8c598cb4d76ca1cd9c2f1f93f
dvdjcw <70393261+dvdjcw@users.noreply.github.com>
2020-09-02 13:09:39 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,26 +8,24 @@ function setup() {
   movingrect=createSprite(400,200,80,40);
   movingrect.shapeColor="green";
   movingrect.debug=true;
-  
-  movingrect.velocityY = -5;
-  fixedrect.velocityY = 5;
 }
 
 function draw() {
   background(255,255,255);
-  //movingrect.x=World.mouseX;
-  //movingrect.y=World.mouseY;
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
   if(movingrect.x - fixedrect.x < fixedrect.width/2 + movingrect.width/2
-    && fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2)
-  {
-    movingrect.velocityX = movingrect.velocityX * (-1);
-    fixedrect.velocityX = fixedrect.velocityX * (-1);
-  }  
-  if(movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2
+    && fixedrect.x - movingrect.x < fixedrect.width/2 + movingrect.width/2
+    && movingrect.y - fixedrect.y < fixedrect.height/2 + movingrect.height/2
     && fixedrect.y - movingrect.y < fixedrect.height/2 + movingrect.height/2)
   {
-    movingrect.velocityY = movingrect.velocityY * (-1);
-    fixedrect.velocityY = fixedrect.velocityY * (-1);
+    movingrect.shapeColor = "red";
+    fixedrect.shapeColor = "red";
+  }  
+  else
+  {
+    movingrect.shapeColor = "green";
+    fixedrect.shapeColor = "green";
   }
   drawSprites();
 }
